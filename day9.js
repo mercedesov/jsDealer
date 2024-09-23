053082b7dc42a275a4cbfe866488f0ccc8bc97a9
ghost <106078876+mercedesov@users.noreply.github.com>
2024-09-23 20:33:34 +0400

    add project file

---
+++ day9.js
@@ -0,0 +1,30 @@
+//Write a function that will calculate the arithmetic mean of the years of an array.
+function calculateMean(arr) {
+  // Sum all elements of the array
+  const sum = arr.reduce((acc, val) => acc + val, 0);
+  
+  // Divide the sum by the length of the array to get the mean
+  const mean = sum / arr.length;
+  
+  return mean;
+}
+
+//testing
+const result = calculateMean([5, 8, 12, 4]);
+console.log(result); 
+
+/* given a function that takes a number as an argument. it is required
+to find the product of the digits of the number received in the
+argument. */
+
+function productOfDigits(num) {
+  let product = 1; 
+  let digits = String(num).split(''); 
+  for (let i = 0; i < digits.length; i++) {
+    product *= Number(digits[i]);   }
+
+  return product;
+}
+
+//testing
+console.log(productOfDigits(321));
